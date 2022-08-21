88fa94bc64c1814df8d086c21c377a790ec05fa4
Arefe Emrani <emrani97@yahoo.com>
2022-08-22 02:04:40 +0300

    Hello and Bye!

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -13,9 +13,9 @@
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-var speakWord = "Hello";
 var helloSpeaker = {};
 helloSpeaker.speak=function(name) {
+  var speakWord = "Hello";
   console.log(speakWord + " " + name);
 };
 
